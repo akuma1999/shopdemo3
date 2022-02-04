464daf539aa20f95b2d7f84f1e871b9da45251ee
nguyen_hoang_lam <santoruy1999@gmail.com>
2022-02-04 10:57:52 +0700

    remake model mongoose

---
+++ pages/api/product.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+	name: string;
+};
+
+export default function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<any>
+) {
+	res.status(201).json({ name: 'John Doe' });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,9 @@ import styles from '../styles/Home.module.css';
 import Head from '../components/Head';
 //
 
-const Home: NextPage = () => {
+const Home: NextPage = (props: any) => {
+	const { data, status } = props;
+	console.log(data, status);
 	return (
 		<>
 			<Head
@@ -18,5 +20,14 @@ const Home: NextPage = () => {
 		</>
 	);
 };
+
+export async function getServerSideProps(context: any) {
+	const res = await fetch('http://localhost:3000/api/product');
+	const data = await res.json();
+	const status = await res.status;
+	return {
+		props: { data, status }, // will be passed to the page component as props
+	};
+}
 
 export default Home;
